ac60414f1a6feab1e607541ac7af889f611fa131
Kodo Yousif <kodo.yousif@gmail.com>
2024-09-25 13:34:20 +0300

    change to div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-4">
+      <div className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-4">
         <div className="flex justify-between">
           <div className="flex-1 space-y-1.5 pr-3 sm:pr-0">
             <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
@@ -200,7 +200,7 @@ export default function Page() {
             })}
           </div>
         </Section>
-      </section>
+      </div>
 
       <CommandMenu
         links={[
